c54d7e0b2c4b643845ec4b0ea4800e413735fad6
NivedhithaV <nivedhithavenugopal@gmail.com>
2016-07-31 01:06:15 -0400

    Update bower

--- karma.conf.js
+++ karma.conf.js
@@ -4,8 +4,8 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
+      'app/vendor/angular/angular.js',
+      'app/vendor/angular/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
